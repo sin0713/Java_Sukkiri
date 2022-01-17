b29b97f5684c4838c831bc4fcac1bae879959eed
sin0713 <narisin1124@gmail.com>
2022-01-17 18:51:11 +0900

    [Add]

--- src/practice01/Main02.java
+++ src/practice01/Main02.java
@@ -1,5 +1,11 @@
 package practice01;
 
 public class Main02 {
-
+	String concatPath(String folder, String file) {
+		if(!folder.endsWith("\\")) {
+			folder += "\\";
+		}
+		
+		return folder + file;
+	}
 }
